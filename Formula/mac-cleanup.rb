12bba37504e23194e0d8a77f3f56018eecb65a6e
Sunnythesage <Sundaypius2000@gmail.com>
2026-03-12 06:00:03 +0100

    chore(formula): bump to v0.2.2

--- Formula/mac-cleanup.rb
+++ Formula/mac-cleanup.rb
@@ -1,8 +1,8 @@
 class MacCleanup < Formula
   desc "Modular macOS developer storage cleanup CLI"
   homepage "https://github.com/PiusSunday/mac-cleanup"
-  url "https://github.com/PiusSunday/mac-cleanup/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "331edf8297b776b0ebb1813e86ae659927482cba3c34063f382040d46baa49c3"
+  url "https://github.com/PiusSunday/mac-cleanup/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "6107bd9e0d6812249db6a501a68f7982dbab31637bfa86257f1095a61596896d"
   license "MIT"
 
   def install
