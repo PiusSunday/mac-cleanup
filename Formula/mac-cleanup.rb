952ac4686cb40e8dc395db819121ab5943e9e0a5
Sunnythesage <Sundaypius2000@gmail.com>
2026-03-12 06:13:28 +0100

    chore(formula): update SHA-256 for v0.2.2

--- Formula/mac-cleanup.rb
+++ Formula/mac-cleanup.rb
@@ -2,7 +2,7 @@ class MacCleanup < Formula
   desc "Modular macOS developer storage cleanup CLI"
   homepage "https://github.com/PiusSunday/mac-cleanup"
   url "https://github.com/PiusSunday/mac-cleanup/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "6107bd9e0d6812249db6a501a68f7982dbab31637bfa86257f1095a61596896d"
+  sha256 "9998fe24538edb31da24175e889a11b57e7e62f9effa98ffa24cd707675279e7"
   license "MIT"
 
   def install
