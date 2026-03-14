5f9887c40094cda2a393fbdec3d151dadb05fd23
Sunnythesage <Sundaypius2000@gmail.com>
2026-03-14 15:19:01 +0100

    chore: update Homebrew formula for v0.4.1 and add release workflow

--- Formula/mac-cleanup.rb
+++ Formula/mac-cleanup.rb
@@ -1,8 +1,8 @@
 class MacCleanup < Formula
   desc "Modular macOS developer storage cleanup CLI"
   homepage "https://github.com/PiusSunday/mac-cleanup"
-  url "https://github.com/PiusSunday/mac-cleanup/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "REPLACE_ME"
+  url "https://github.com/PiusSunday/mac-cleanup/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "89797863637a4ad418768179ab121f591bc8a19da9f5dd5c9dc1025927fe9f1e"
   license "MIT"
 
   def install
